689d22bda76efb545ec72c99dbe01ca47d6b7fce
PierreLgol <plgol.perso@gmail.com>
2025-04-22 17:56:22 +0200

    first program

---
+++ day00/ex00/Hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Hello.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pollivie <plgol.perso@gmail.com>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/04/22 15:10:49 by pollivie          #+#    #+#              #
+#    Updated: 2025/04/22 15:10:49 by pollivie         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "France!")
+ft_set = {"Hello", "Paris!"}
+ft_dict = {"Hello": "42Paris!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
