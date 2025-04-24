119875d2afb82c3d354c34ba445060fb430440c3
PierreLgol <plgol.perso@gmail.com>
2025-04-24 10:48:45 +0200

    fixed ex02

--- day00/ex02/find_ft_type.py
+++ day00/ex02/find_ft_type.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    find_ft_type.py                                    :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: pollivie <plgol.perso@gmail.com>           +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/04/22 15:11:27 by pollivie          #+#    #+#              #
-#    Updated: 2025/04/22 15:11:27 by pollivie         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+
+def isNumber(object: any) -> bool:
+    """This function returns true if 'object' is of type 'int | float'"""
+    return isinstance(object, int | float)
+
+
+def isString(object: any) -> bool:
+    """This function returns true if 'object' is of type 'str'"""
+    return isinstance(object, str)
+
+
+def getCapitalizedName(object: any) -> str:
+    """This function returns the capitalized name of any given type"""
+    return type(object).__name__.capitalize()
 
 
 def all_thing_is_obj(object: any) -> int:
-    if isinstance(object, str):
-        print(f"{object} is in the kitchen : {type(object)}")
-    elif isinstance(object, int):
+    """This function display 'object' differently depending on it's type"""
+    if isNumber(object):
         print("Type not found")
+    elif isString(object):
+        print(f"{object} is in the kitchen : {type(object)}")
     else:
-        print(f"{type(object).__name__.capitalize()} : {type(object)}")
-
+        print(f"{getCapitalizedName(object)} : {type(object)}")
     return 42
