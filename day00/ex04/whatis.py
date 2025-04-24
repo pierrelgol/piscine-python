1f6c6f03d6c8839674c69aa24cb9420b1fdb600d
PierreLgol <plgol.perso@gmail.com>
2025-04-24 11:43:51 +0200

    ex04

--- day00/ex04/whatis.py
+++ day00/ex04/whatis.py
@@ -1,43 +1,62 @@
-#!/usr/bin/env python3
-
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    whatis.py                                          :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: pollivie <plgol.perso@gmail.com>           +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/04/22 16:06:22 by pollivie          #+#    #+#              #
-#    Updated: 2025/04/22 16:06:22 by pollivie         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+#!/usr/bin/env python
 
 import sys as sys
 
 
-def isEvenOrOddStr(number: any) -> str:
-    if abs(int(number)) & 1 == 0:
-        print("I'm Even")
+def isEven(number: int) -> bool:
+    """Returns 'True' if number is even"""
+    return abs(number) & 1 == 0
+
+
+def intFromString(number: str) -> int | None:
+    """
+    Returns an 'int' if 'number' conversion is safe or 'None' if converting
+    'number' would raise an exception
+    """
+    if number.isnumeric():
+        return int(number)
     else:
-        print("I'm Odd")
+        return None
+
+
+def eprintln(exception: AssertionError) -> None:
+    """Print an 'AssertionError' to stderr."""
+    print(exception, file=sys.stderr)
+
+
+def errorAndDie(exception: AssertionError) -> None:
+    """Print an 'AssertionError' to stderr. And exit with status code '1'"""
+    eprintln(exception)
+    sys.exit(1)
+
+
+def printResult(is_even: bool) -> None:
+    """Prints I'm Even if is even else I'm Odd."""
+    if is_even:
+        print("I'm Even.")
+    else:
+        print("I'm Odd.")
 
 
 def main() -> int:
-    argc: int = len(sys.argv)
-
-    if argc == 1:
+    """The program entry point. If that wasn't fucking obvious."""
+    if len(sys.argv) == 1:
         return 0
-    if argc > 2:
-        print("AssertionError: more than one argument is provided")
-        return 1
 
     try:
-        number: int = int(sys.argv[1])
-        isEvenOrOddStr(number=number)
-    except Exception:
-        print("AssertionError: argument is not an integer")
-        return 1
+        if len(sys.argv) > 2:
+            raise AssertionError("AssertionError: more than one argument is provided")
+    except AssertionError as ae:
+        errorAndDie(ae)
 
+    try:
+        number: int | None = intFromString(number=sys.argv[1])
+        if number is None:
+            raise AssertionError("AssertionError: argument is not an integer")
+        else:
+            printResult(is_even=isEven(number=number))
+    except AssertionError as ae:
+        errorAndDie(ae)
     return 0
 
 
