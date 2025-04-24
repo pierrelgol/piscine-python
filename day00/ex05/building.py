0156efa568940211e72677318c04e8e17ac1368d
PierreLgol <plgol.perso@gmail.com>
2025-04-24 12:31:38 +0200

    ex05

--- day00/ex05/building.py
+++ day00/ex05/building.py
@@ -2,59 +2,92 @@
 import sys as sys
 
 
-def countText(input) -> None:
-    # This function counts each characters by kind
+def eprintln(exception: AssertionError) -> None:
+    """Reports an AssertionError to 'stderr'"""
+    print(exception, file=sys.stderr)
 
-    total_lower: int = 0
-    total_upper: int = 0
-    total_space: int = 0
-    total_punct: int = 0
-    total_value: int = 0
-    total_digit: int = 0
+
+def errorAndDie(exception: AssertionError) -> None:
+    """Reports an AssertionError to 'stderr' and exit with status code 1"""
+    eprintln(exception=exception)
+    sys.exit(1)
+
+
+def getUserInput() -> str:
+    """Returns a string of user inputs from sys.argv[1] or from 'input()'"""
+    if len(sys.argv) == 1:
+        input_str: str = ""
+        try:
+            input_str = input("What is the text to count?")
+            input_str += "\n"
+        except EOFError:
+            return input_str
+        except KeyboardInterrupt:
+            return input_str + " "
+        return input_str
+    else:
+        return sys.argv[1]
+
+
+def countText(input: str) -> dict[str:int]:
+    """
+    Returns a dict[str:int] that contains a distinct count of letters in input
+    by the following kind:
+        - "lower",
+        - "upper",
+        - "space",
+        - "punct",
+        - "value",
+        - "digit",
+    """
+    count: dict[str:int] = {
+        "lower": 0,
+        "upper": 0,
+        "space": 0,
+        "punct": 0,
+        "value": 0,
+        "digit": 0,
+    }
 
     for char in input:
         if char.islower():
-            total_lower += 1
+            count["lower"] += 1
         elif char.isupper():
-            total_upper += 1
+            count["upper "] += 1
         elif char.isspace():
-            total_space += 1
+            count["space"] += 1
         elif char in "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~":
-            total_punct += 1
+            count["punct"] += 1
         elif char.isdigit():
-            total_digit += 1
-        total_value += 1
-
-    print(f"The text contains {total_value} characters:")
-    print(f"{total_upper} letters")
-    print(f"{total_lower} letters")
-    print(f"{total_punct} punctuation marks")
-    print(f"{total_space} spaces")
-    print(f"{total_digit} digits")
+            count["digit"] += 1
+        count["value"] += 1
+    return count
 
 
-def promptUser() -> None:
-    # This function prompts the user for inputs.
-    print("What is the text to count?")
-    input_str: str = ""
-    try:
-        input_str = input()
-        input_str += "\n"
-    except EOFError:
-        return countText(input_str)
-    except KeyboardInterrupt:
-        return countText(input_str + " ")
-    return countText(input_str)
+def printResult(results: dict[str:int]) -> None:
+    """
+    This function display the keys of results and their values
+    """
+    print("The text contains")
+    print(f"{results['value']} characters:")
+    print(f"{results['upper']} letters")
+    print(f"{results['lower']} letters")
+    print(f"{results['punct']} punctuation marks")
+    print(f"{results['space']} spaces")
+    print(f"{results['digit']} digits")
 
 
 def main() -> int:
-    # Program entry point
-    if len(sys.argv) == 1:
-        promptUser()
-    elif len(sys.argv) > 2:
-        print("AssertionError: more than one argument is provided")
-    else:
-        countText(sys.argv[1])
+    """program entry point"""
+    try:
+        if len(sys.argv) > 2:
+            raise AssertionError("AssertionError: more than one argument is provided")
+        else:
+            printResult(countText(getUserInput()))
+    except AssertionError as ae:
+        errorAndDie(ae)
+    except Exception as e:
+        errorAndDie(e)
     return 0
 
 
