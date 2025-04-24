0e073462484b566955fe0c3901c6b8220772e080
PierreLgol <plgol.perso@gmail.com>
2025-04-24 14:34:47 +0200

    implemented ex09 and added descriptiion

--- day00/ex09/ft_package/ft_package/ft_package.py
+++ day00/ex09/ft_package/ft_package/ft_package.py
@@ -2,4 +2,5 @@
 
 
 def count_in_list(items: list[str] = [], item: str = "") -> int:
+    """This function returns the number of 'item' in 'items'"""
     return [i for i in items if i == item].count(item)
