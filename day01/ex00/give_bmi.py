9ad7fe6fa785108539800c4099f3a5dd9fc3d4e8
PierreLgol <plgol.perso@gmail.com>
2025-04-24 15:21:31 +0200

    finished ex00

--- day01/ex00/give_bmi.py
+++ day01/ex00/give_bmi.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
-# from typing import List, Tuple
-
 
 def give_bmi(h8: list[int | float], w8: list[int | float]) -> list[int | float]:
+    """This function returns a list of 'bmi'"""
     return [w / (h**2) for h, w in list(zip(h8, w8))]
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    return list(map(lamda x))
+    """This function returns a list of bool for each value in 'bmi 'if its above 'limit'"""
+    results: list[bool] = []
+    for i, b in enumerate(bmi):
+        results.append(b > limit)
+    return results
