8b90728fa215f7fbfa068f840a7051775f04bdc7
PierreLgol <plgol.perso@gmail.com>
2025-04-24 12:57:25 +0200

    reworked ex07

--- day00/ex07/sos.py
+++ day00/ex07/sos.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-from typing import Dict, List, NoReturn
 
 
-def getMorseMap() -> Dict[str, str]:
-    # provides the Morse map
+def getMorseMap() -> dict[str, str]:
+    """Returns a map of morse code mapping"""
     # fmt: off
-    morse_map: Dict[str, str] = {
+    morse_map: dict[str, str] = {
         'A': '.-',     'B': '-...',   'C': '-.-.',   'D': '-..',
         'E': '.',      'F': '..-.',   'G': '--.',    'H': '....',
         'I': '..',     'J': '.---',   'K': '-.-',    'L': '.-..',
@@ -24,27 +23,45 @@ def getMorseMap() -> Dict[str, str]:
     return morse_map
 
 
-def error_and_die() -> NoReturn:
-    # function that terminates in case of erros
-    print("AssertionError: the arguments are bad")
+def eprintln(exception: any) -> None:
+    """Reports an AssertionError to stderr"""
+    print(exception, file=sys.stderr)
+
+
+def error_and_die(exception: AssertionError) -> None:
+    """Reports an AssertionError to stderr and exits with status code '1'"""
+    eprintln(exception=exception)
     sys.exit(1)
 
 
 def fromStringToMorseCode(input: str) -> str:
-    # conversion function
+    """Returns a string converting any alphanumeric into morse code"""
     to_morse = getMorseMap()
-    morsed: List[str] = []
+    morsed: list[str] = []
     for char in input:
         upper_char = char.upper()
-        if upper_char not in to_morse:
-            error_and_die()
-        morsed.append(to_morse[upper_char])
+        try:
+            if upper_char not in to_morse:
+                raise AssertionError("AssertionError: the arguments are bad")
+            else:
+                morsed.append(to_morse[upper_char])
+        except AssertionError as ae:
+            error_and_die(ae)
+        except Exception as e:
+            error_and_die(e)
     return " ".join(morsed)
 
 
 def main() -> None:
-    if len(sys.argv) != 2:
-        error_and_die()
+    """Program entry point"""
+    try:
+        if len(sys.argv) != 2:
+            raise AssertionError("AssertionError: the arguments are bad")
+    except AssertionError as ae:
+        error_and_die(ae)
+    except Exception as e:
+        error_and_die(e)
+
     print(fromStringToMorseCode(sys.argv[1]))
 
     return
